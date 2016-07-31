ae856012750051757c52e38aa022a6b59359b975
Michael Walker <mike@barrucadu.co.uk>
2016-07-31 13:15:43 +0100

    Add a wrapper around raft.LogStore.

---
+++ raft/raft.go
@@ -0,0 +1,147 @@
+// Package raft provides a wrapper making a 'LogDB' instance
+// appropriate for use as a 'LogStore' for the
+// github.com/hashicorp/raft library.
+package raft
+
+import (
+	"bytes"
+
+	"github.com/barrucadu/logdb"
+
+	"github.com/hashicorp/raft"
+	"github.com/ugorji/go/codec"
+)
+
+// LogStore is implements the hashicorp/raft 'LogStore' interface,
+// with the backing store being a 'LogDB'.
+type LogStore struct {
+	Store  logdb.LogDB
+	Handle codec.Handle
+}
+
+// UsingBinc creates a new 'LogStore' using binc to encode and decode
+// log entries.
+func UsingBinc(store logdb.LogDB) *LogStore {
+	h := new(codec.BincHandle)
+	h.ErrorIfNoField = true
+	h.InternString = true
+
+	return &LogStore{
+		Store:  store,
+		Handle: h,
+	}
+}
+
+// UsingCBOR creates a new 'LogStore' using CBOR to encode and decode
+// log entries.
+func UsingCBOR(store logdb.LogDB) *LogStore {
+	h := new(codec.CborHandle)
+	h.ErrorIfNoField = true
+	h.InternString = true
+
+	return &LogStore{
+		Store:  store,
+		Handle: h,
+	}
+}
+
+// UsingJSON creates a new 'LogStore' using JSON to encode and decode
+// log entries.
+func UsingJSON(store logdb.LogDB) *LogStore {
+	h := new(codec.JsonHandle)
+	h.ErrorIfNoField = true
+	h.InternString = true
+
+	return &LogStore{
+		Store:  store,
+		Handle: h,
+	}
+}
+
+// UsingMsgpack creates a new 'LogStore' using msgpack to encode and
+// decode log entries.
+func UsingMsgpack(store logdb.LogDB) *LogStore {
+	h := new(codec.MsgpackHandle)
+	h.ErrorIfNoField = true
+	h.InternString = true
+
+	return &LogStore{
+		Store:  store,
+		Handle: h,
+	}
+}
+
+// UsingSimple creates a new 'LogStore' using simple to encode and
+// decode log entries.
+func UsingSimple(store logdb.LogDB) *LogStore {
+	h := new(codec.SimpleHandle)
+	h.ErrorIfNoField = true
+	h.InternString = true
+
+	return &LogStore{
+		Store:  store,
+		Handle: h,
+	}
+}
+
+// FirstIndex returns the first index written. 0 for no entries.
+func (l *LogStore) FirstIndex() (uint64, error) {
+	return l.Store.OldestID(), nil
+}
+
+// LastIndex returns the last index written. 0 for no entries.
+func (l *LogStore) LastIndex() (uint64, error) {
+	next := l.Store.NextID()
+	if next == 0 {
+		return 0, nil
+	}
+	return next - 1, nil
+}
+
+// GetLog gets a log entry at a given index.
+func (l *LogStore) GetLog(index uint64, log *raft.Log) error {
+	bs, err := l.Store.Get(index)
+	if err != nil {
+		return raft.ErrLogNotFound
+	}
+	return codec.NewDecoderBytes(bs, l.Handle).Decode(log)
+}
+
+// StoreLog stores a log entry.
+func (l *LogStore) StoreLog(log *raft.Log) error {
+	buf := new(bytes.Buffer)
+	if err := codec.NewEncoder(buf, l.Handle).Encode(*log); err != nil {
+		return err
+	}
+	return l.Store.Append(buf.Bytes())
+}
+
+// StoreLogs stores multiple log entries.
+func (l *LogStore) StoreLogs(logs []*raft.Log) error {
+	bss := make([][]byte, len(logs))
+	for i, log := range logs {
+		buf := new(bytes.Buffer)
+		if err := codec.NewEncoder(buf, l.Handle).Encode(*log); err != nil {
+			return err
+		}
+		bss[i] = buf.Bytes()
+	}
+	return l.Store.AppendEntries(bss)
+}
+
+// DeleteRange deletes a range of log entries. The range is inclusive.
+//
+// This makes use of the fact that this can be turned into a Forget or
+// Rollback: deletion is always from one end.
+func (l *LogStore) DeleteRange(min, max uint64) error {
+	if min <= l.Store.OldestID() {
+		return l.Store.Forget(max)
+	}
+	return l.Store.Rollback(min)
+}
+
+// Close the underlying database. It is not safe to use the 'LogStore'
+// for anything else after doing this.
+func (l *LogStore) Close() error {
+	return l.Store.Close()
+}
---
+++ raft/raft_test.go
@@ -0,0 +1,201 @@
+package raft
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/barrucadu/logdb"
+
+	"github.com/hashicorp/raft"
+	"github.com/stretchr/testify/assert"
+)
+
+const numEntries = 255
+
+func TestEmptyIndices(t *testing.T) {
+	db := assertCreate(t, "empty_indices")
+	defer assertClose(t, db)
+
+	assert.Equal(t, uint64(0), assertFirstIndex(t, db))
+	assert.Equal(t, uint64(0), assertLastIndex(t, db))
+}
+
+func TestStoreLog(t *testing.T) {
+	db := assertCreate(t, "store_log")
+	defer assertClose(t, db)
+
+	logs := make([]*raft.Log, numEntries)
+	for i := 0; i < len(logs); i++ {
+		logs[i] = &raft.Log{
+			Index: uint64(i),
+			Term:  0,
+			Type:  raft.LogType(i),
+			Data:  []byte(fmt.Sprintf("log entry %v", i)),
+		}
+	}
+
+	for _, log := range logs {
+		assertStoreLog(t, db, log)
+	}
+
+	assert.Equal(t, uint64(1), assertFirstIndex(t, db))
+	assert.Equal(t, uint64(len(logs)), assertLastIndex(t, db))
+
+	for i, log := range logs {
+		l := assertGetLog(t, db, uint64(i+1))
+		assert.Equal(t, *log, *l)
+	}
+}
+
+func TestStoreLogs(t *testing.T) {
+	db := assertCreate(t, "store_logs")
+	defer assertClose(t, db)
+
+	logs := make([]*raft.Log, numEntries)
+	for i := 0; i < len(logs); i++ {
+		logs[i] = &raft.Log{
+			Index: uint64(i),
+			Term:  0,
+			Type:  raft.LogType(i),
+			Data:  []byte(fmt.Sprintf("log entry %v", i)),
+		}
+	}
+
+	assertStoreLogs(t, db, logs)
+
+	assert.Equal(t, uint64(1), assertFirstIndex(t, db))
+	assert.Equal(t, uint64(len(logs)), assertLastIndex(t, db))
+
+	for i, log := range logs {
+		l := assertGetLog(t, db, uint64(i+1))
+		assert.Equal(t, *log, *l)
+	}
+}
+
+func TestDeleteRangeFromStart(t *testing.T) {
+	db := assertCreate(t, "delete_range_from_start")
+	defer assertClose(t, db)
+
+	logs := filldb(t, db)
+
+	first := assertFirstIndex(t, db)
+	toDelete := uint64(50)
+	assertDeleteRange(t, db, first, first+toDelete)
+
+	assert.Equal(t, first+toDelete, assertFirstIndex(t, db))
+
+	for i, log := range logs {
+		index := uint64(i) + 1
+		if index <= toDelete {
+			if err := db.GetLog(index, new(raft.Log)); err == nil {
+				t.Fatal("expected log entry to be missing")
+			}
+		} else {
+			l := assertGetLog(t, db, index)
+			assert.Equal(t, *log, *l)
+		}
+	}
+}
+
+func TestDeleteRangeFromEnd(t *testing.T) {
+	db := assertCreate(t, "delete_range_from_end")
+	defer assertClose(t, db)
+
+	logs := filldb(t, db)
+
+	last := assertLastIndex(t, db)
+	toDelete := uint64(50)
+	assertDeleteRange(t, db, last-toDelete, last)
+
+	assert.Equal(t, last-toDelete-1, assertLastIndex(t, db))
+
+	for i, log := range logs {
+		index := uint64(i) + 1
+		if index >= last-toDelete {
+			if err := db.GetLog(index, new(raft.Log)); err == nil {
+				t.Fatal("expected log entry to be missing ")
+			}
+		} else {
+			l := assertGetLog(t, db, index)
+			assert.Equal(t, *log, *l)
+		}
+	}
+}
+
+/// ASSERTIONS
+
+func assertCreate(t *testing.T, testName string) *LogStore {
+	_ = os.RemoveAll("../test_db/raft/" + testName)
+	db, err := logdb.Create("../test_db/raft/"+testName, 0, 1024)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return UsingMsgpack(db)
+}
+
+func assertClose(t *testing.T, db *LogStore) {
+	if err := db.Close(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func assertFirstIndex(t *testing.T, db *LogStore) uint64 {
+	index, err := db.FirstIndex()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return index
+}
+
+func assertLastIndex(t *testing.T, db *LogStore) uint64 {
+	index, err := db.LastIndex()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return index
+}
+
+func assertGetLog(t *testing.T, db *LogStore, index uint64) *raft.Log {
+	log := new(raft.Log)
+	if err := db.GetLog(index, log); err != nil {
+		t.Fatal(err)
+	}
+	return log
+}
+
+func assertStoreLog(t *testing.T, db *LogStore, log *raft.Log) {
+	if err := db.StoreLog(log); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func assertStoreLogs(t *testing.T, db *LogStore, logs []*raft.Log) {
+	if err := db.StoreLogs(logs); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func assertDeleteRange(t *testing.T, db *LogStore, min, max uint64) {
+	if err := db.DeleteRange(min, max); err != nil {
+		t.Fatal(err)
+	}
+}
+
+/// UTILITIES
+
+func filldb(t *testing.T, db *LogStore) []*raft.Log {
+	logs := make([]*raft.Log, numEntries)
+	for i := 0; i < len(logs); i++ {
+		logs[i] = &raft.Log{
+			Index: uint64(i),
+			Term:  0,
+			Type:  raft.LogType(i),
+			Data:  []byte(fmt.Sprintf("log entry %v", i)),
+		}
+	}
+
+	assertStoreLogs(t, db, logs)
+
+	return logs
+}
